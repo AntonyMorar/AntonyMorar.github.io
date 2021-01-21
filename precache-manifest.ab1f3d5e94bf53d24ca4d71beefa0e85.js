141ab5a30823b20a6a4f039e85059ee38b4f07ef
antony999k <antony999k@hotmail.com>
2021-01-21 15:35:33 -0600

    :sparkles: Cambios menores

---
+++ precache-manifest.ab1f3d5e94bf53d24ca4d71beefa0e85.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d47e0a424ff90e05960a27c86ec7636",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75bdad739c9d574510e6",
+    "url": "/static/css/main.a9417eda.chunk.css"
+  },
+  {
+    "revision": "6e9fdfe2b99d59a26381",
+    "url": "/static/js/2.0f763a2d.chunk.js"
+  },
+  {
+    "revision": "8a212da79dd56e79ad3bc5949d5f5ac0",
+    "url": "/static/js/2.0f763a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bdad739c9d574510e6",
+    "url": "/static/js/main.0bce9c39.chunk.js"
+  },
+  {
+    "revision": "c680ded3c48af0cf32e4",
+    "url": "/static/js/runtime-main.1a7a0475.js"
+  },
+  {
+    "revision": "a6509aa3161205ebe01c0f7e2f209b44",
+    "url": "/static/media/LeagueSpartan-Bold.a6509aa3.otf"
+  },
+  {
+    "revision": "6f90b88e740da627c7242cd530699738",
+    "url": "/static/media/SolarSystemGif.6f90b88e.gif"
+  },
+  {
+    "revision": "5eea0e78967d6a7a43eb799cff72210f",
+    "url": "/static/media/bodyrescue.5eea0e78.png"
+  },
+  {
+    "revision": "901c967d4e1c333415cdbbf1a03d1976",
+    "url": "/static/media/happytetris.901c967d.png"
+  },
+  {
+    "revision": "bf097ccd2a4c71bb9e16bdc3bd7a9be4",
+    "url": "/static/media/happytetrisBG.bf097ccd.png"
+  },
+  {
+    "revision": "b94e91aaab71b6af88ea5296ce9deb4a",
+    "url": "/static/media/inordie.b94e91aa.png"
+  },
+  {
+    "revision": "5c294e92a4ea4dd9f06c6758741fec68",
+    "url": "/static/media/kip.5c294e92.png"
+  },
+  {
+    "revision": "b2b361a44543bd09b9f606f0d1555e4a",
+    "url": "/static/media/neonwars.b2b361a4.png"
+  },
+  {
+    "revision": "e29b96714b361b66d0f5b015df12c20a",
+    "url": "/static/media/neonwarsBG.e29b9671.png"
+  },
+  {
+    "revision": "842b66531404a0421fee764b6b4631b0",
+    "url": "/static/media/solarsystem.842b6653.png"
+  },
+  {
+    "revision": "645a819797b50367494885976f019217",
+    "url": "/static/media/solarsystemBG.645a8197.png"
+  },
+  {
+    "revision": "aca990ba4e9997daa16e32e0968c1718",
+    "url": "/static/media/viewOnGithub.aca990ba.png"
+  }
+]);
